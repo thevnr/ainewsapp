e2323d59c7a1a42800d923b10f8e2c0457388e98
thevnr <42862997+thevnr@users.noreply.github.com>
2021-07-21 20:21:16 +0530

    Add files via upload

---
+++ src/components/NewsCards/InfoCards.js
@@ -0,0 +1,29 @@
+const infoCards = [
+  { color: "#00838f", title: "Latest News", text: "Give me the latest news" },
+  {
+    color: "#00838f",
+    title: "News Categories",
+    info:
+      "Business, Entertainment, General, Health, Science, Sports, Technology",
+    text: "Give me the latest Technology news",
+  },
+  {
+    color: "#00838f",
+    title: "News Terms",
+    info: "Bitcoin, PlayStation 5, Smartphones, Donald Trump...",
+    text: "What's up with Coronavirus",
+  },
+  {
+    color: "#00838f",
+    title: "News Sources",
+    info: "Wired, BBC News, Time, IGN, CNN, Buzzfeed, ABC News...",
+    text: "Give me the news from BBC News",
+  },
+  {
+    color: "#00838f",
+    title: "News Countries",
+    info: "China, United States, Germany, Saudi Arabia, Australia...",
+    text: "Top headlines from India",
+  },
+];
+export default infoCards;
